9125437f070b8696406f5bef2d8abccfaa5ea89d
YutoMueller <136407240+yutomueller@users.noreply.github.com>
2026-03-13 21:06:19 +0900

    Update run_all.bat

--- run_all.bat
+++ run_all.bat
@@ -1,4 +1,4 @@
 @echo off
-start cmd /k "cd /d %~dp0backend && python -m venv .venv && .venv\Scriptsctivate && pip install -r requirements.txt && uvicorn app.main:app --reload --port 8000"
+start cmd /k "cd /d %~dp0backend && python -m venv .venv && .venv\Scripts\activate && pip install -r requirements.txt && uvicorn app.main:app --reload --port 8000"
 start cmd /k "cd /d %~dp0frontend && npm install && npm run dev"
 start http://localhost:5173
